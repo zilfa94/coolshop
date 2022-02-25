8654284a6f9bc540fceae4eb5b05bf84be8d73be
fazil <fnahnah@gmail.com>
2022-02-26 00:45:19 +0100

    coolshopv4

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import getCommerce from '../utils/commerce'
+
+import Link from 'next/link';
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Slide,
+  Typography,
+} from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import Layout from '../components/Layout';
+import getCommerce from '../utils/commerce';
 
 export default function Home(props) {
   const { products } = props;
   return (
     
       <Layout title="Home" commercePublicKey={props.commercePublicKey}>
-          <main className={styles.main}>
-            {products.map((product) => (
-              <div key={product.id}>
-                <img src={product.image.url} alt={product.name} />
-                <p>{product.name}</p>
-                <p>{product.price.formatted_with_symbol}</p>
-
-              </div>
-            ))}
-          </main>
+        {products.length === 0 && <Alert>No product found</Alert>}
+        <Grid container spacing={1}>
+        {products.map((product) => (
+          <Grid key={product.id} item md={3}>
+            <Slide direction="up" in={true}>
+              <Card>
+                <Link href={`/products/${product.permalink}`}>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      alt={product.name}
+                      image={product.image.url}
+                    />
+                    <CardContent>
+                      <Typography
+                        gutterBottom
+                        variant="body2"
+                        color="textPrimary"
+                        component="p"
+                      >
+                        {product.name}
+                      </Typography>
+                      <Box>
+                        <Typography
+                          variant="body1"
+                          color="textPrimary"
+                          component="p"
+                        >
+                          {product.price.formatted_with_symbol}
+                        </Typography>
+                      </Box>
+                    </CardContent>
+                  </CardActionArea>
+                </Link>
+              </Card>
+            </Slide>
+          </Grid>
+        ))}
+      </Grid>
       </Layout>
 
 
